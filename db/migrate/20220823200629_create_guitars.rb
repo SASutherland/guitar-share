338ba6ee905e95adb0ca90c96616c7ee8609eeb4
Shawn Sutherland <shawnsutherland@hotmail.com>
2022-08-23 22:07:12 +0200

    Added gems.

---
+++ db/migrate/20220823200629_create_guitars.rb
@@ -0,0 +1,14 @@
+class CreateGuitars < ActiveRecord::Migration[7.0]
+  def change
+    create_table :guitars do |t|
+      t.string :brand
+      t.string :model
+      t.string :color
+      t.string :fretboard
+      t.integer :price
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
